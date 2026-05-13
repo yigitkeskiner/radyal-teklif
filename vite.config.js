ce47655ac9d1c4dea1ce2641d180a44e8dad0a86
yiğit keskiner <76444003+yigitkeskiner@users.noreply.github.com>
2026-05-13 11:07:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Radyal Teklif - v1.0
 export default defineConfig({
   plugins: [react()],
   base: '/radyal-teklif/',
